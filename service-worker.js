87b33bdf4cc9ae7cb551441ee4f0b182550ea1eb
syudhas <venomr@in.com>
2024-12-15 21:01:11 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
 const CACHE_NAME = "ratemate-cache-v1";
 const urlsToCache = [
     "/",
-    "/index.html",
-    "/icons/icon-192.png",
-    "/icons/icon-512.png",
+    "index.html",
+    "icons/icon-192.png",
+    "icons/icon-512.png",
     "https://cdn.tailwindcss.com",
     "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css",
     "https://code.jquery.com/jquery-3.6.0.min.js"
