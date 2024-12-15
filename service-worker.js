b8f6fe300b9f1a4ec32161045c473bf243e30dc7
syudhas <venomr@in.com>
2024-12-15 20:54:24 +0700

    code update; added PWA;

---
+++ service-worker.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = "ratemate-cache-v1";
+const urlsToCache = [
+    "/",
+    "/index.html",
+    "/icons/icon-192.png",
+    "/icons/icon-512.png",
+    "https://cdn.tailwindcss.com",
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css",
+    "https://code.jquery.com/jquery-3.6.0.min.js"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => response || fetch(event.request))
+    );
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames =>
+            Promise.all(
+                cacheNames
+                    .filter(name => name !== CACHE_NAME)
+                    .map(name => caches.delete(name))
+            )
+        )
+    );
+});
